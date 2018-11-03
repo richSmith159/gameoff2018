d97b9efef119fd2cf9fc6152c5242c3d005189d2
Richard Smith <thunderstone.dev@gmail.com>
2018-11-03 11:43:20 +0000

    display blank gameplay screen

---
+++ Blast/GameplayScreen.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Tempest\Window.h>
+#include <Tempest\IGameScreen.h>
+#include <Tempest\GUI.h>
+#include <Tempest\Camera2D.h>
+
+class GameplayScreen : public Tempest::IGameScreen
+{
+public:
+	GameplayScreen(Tempest::Window* window);
+	~GameplayScreen();
+
+	virtual int getNextIndex() const override;
+	virtual int getPrevIndex() const override;
+	virtual void build() override;
+	virtual void destroy() override;
+	virtual void onEntry() override;
+	virtual void onExit() override;
+	virtual void update(float deltaTime) override;
+	virtual void draw() override;
+
+private:
+
+	void initUI();
+	void checkInput();
+
+	Tempest::Window* m_window;
+	Tempest::GUI m_gui;
+	Tempest::Camera2D m_camera;
+
+};
---
+++ Blast/ScreenIndices.h
@@ -0,0 +1,5 @@
+#pragma once
+
+const int NO_SCREEN = -1;
+const int MAIN_MENU = 0;
+const int GAMEPLAY_SCREEN = 1;
